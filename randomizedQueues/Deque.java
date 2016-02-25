36888b51132785002bd305b784f9601d28b2abbb
Vinh Tran <daivinhtran.vt@gmail.com>
2016-02-25 16:19:31 -0500

    first commit

---
+++ randomizedQueues/Deque.java
@@ -0,0 +1,89 @@
+
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Deque is a generalization of a stack and a queue that supports adding
+ * and removing items from either front or the back of the data structure
+ * @author Vinh Tran
+ */
+
+public class Deque<Item> implements Iterable<Item> {
+
+	/**
+	 * Constructs an empty deque
+	 */
+	public Deque() {
+
+	}
+	/**
+	 * Returns true if the deque is empty
+	 * @return <tt>true</tt> if this deque is empty; <tt>false</tt> otherwise
+	 */
+	public boolean isEmpty() {
+
+	}
+	/**
+	 * Returns the number of items on the deque
+	 * @return the number of items on the deque
+	 */
+	public int size() {
+
+	}
+
+	/**
+	 * Adds the item to the front of this deque
+	 * @param item the item to add
+	 */
+	public void addFirst(Item item) {
+
+	}
+	/**
+	 * Adds the item to the back of this deque
+	 * @param item the item to add
+	 */	
+	public void addLast(Item item) {
+
+	}
+	/**
+	 * Remove the first item of the deque
+	 * @return the removed item
+	 */	
+	public Item removeFirst() {
+
+	}
+	/**
+	 * Remove the last item of the deque
+	 * @return the removed item
+	 */	
+	public Item removeLast() {
+
+	}
+	/**
+	 * Return an interator over items in order from front to end
+	 * @return an interator over items in order from front to end
+	 */
+	public Iterator<Item> iterator() {
+
+	}
+	/**
+	 * Unit tests the <tt>Deque</tt> datat type
+	 */
+	public static void main(String[] args) {
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ randomizedQueues/RandomizedQueue.java
@@ -0,0 +1,74 @@
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * A randomized queue is similar to a stack or queue, except the item removed
+ * is chosen uniformly at random from items in the data structure
+ * @author Vinh Tran
+ */
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+	/**
+	 * Constructs an empty randomized queue
+	 */
+	public RandomizedQueue() {
+
+	}
+	/**
+	 * Returns true if the queue is empty
+	 * @return <tt>true</tt> if this queue is empty; <tt>false</tt> otherwise
+	 */
+	public boolean isEmpty() {
+
+	}
+	/**
+	 * Returns the number of items on the queue
+	 * @return the number of items on the queue
+	 */
+	public int size() {
+
+	}
+
+	/**
+	 * Adds the item
+	 * @param item the item to add
+	 */
+	public void enqueue(Item item) {
+
+	}
+	/**
+	 * Remove and return a random item
+	 * @return the removed item
+	 */	
+	public Item dequeue() {
+
+	}
+	/**
+	 * Return an interator over items in order from front to end
+	 * @return an interator over items in order from front to end
+	 */
+	public Iterator<Item> iterator() {
+
+	}
+	/**
+	 * Unit tests the <tt>Queue</tt> datat type
+	 */
+	public static void main(String[] args) {
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
